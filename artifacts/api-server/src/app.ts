f1a51a8ea5a9a468fa507cd88ca12f74c7218f6e
jobseekerpath-gif <jobseeker.path@gmail.com>
2026-08-01 17:06:35 +0000

    Fix secure cookies

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -62,7 +62,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env["NODE_ENV"] === "production",
+      secure: true,
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
       sameSite: "lax",
